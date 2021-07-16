28bb039a245e04631632b18cdee271611467bd94
Yash Sonalia <sonaliayash@gmail.com>
2021-07-16 19:56:56 +0530

    Bootstrap render error fix

--- src/js/controller.js
+++ src/js/controller.js
@@ -2,7 +2,7 @@
 import * as model from "./model.js";
 // import { amountRatioMultiplier } from "./helpers.js";
 import ConversionRatesView from "./views/ConversionRatesView.js";
-import "core-js/stable";
+// import "core-js/stable";
 const controlDataflow = () => {
 	ConversionRatesView.renderTableData(model.simulatedAmount, model.simulatedResponse);
 };
